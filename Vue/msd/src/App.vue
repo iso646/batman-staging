7ce184cf986ae5f11de3974a71db1eb868bb03ce
Batman <batman@ig>
2019-11-15 11:22:59 -0500

    setup complete for new direction

--- Vue/msd/src/App.vue
+++ Vue/msd/src/App.vue
@@ -1,26 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-
     <nav class="main-nav">
-     <div class="logo">
-       my.company
-     </div>
-     <Burger></Burger>
-   </nav>
-
-   <Sidebar>
-     <ul class="sidebar-panel-nav">
-       <li><a href="#home">Home</a></li>
-       <li><a href="#about">About</a></li>
-       <li><a href="#contact">Contact</a></li>
-     </ul>
-   </Sidebar>
-
-
+      <div class="logo">INSIGHT GLOBAL</div>
+      <Burger></Burger>
+    </nav>
+    <Sidebar>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/welcome">Welcome</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+    </Sidebar>
     <router-view/>
   </div>
 </template>
